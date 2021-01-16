410ddce73a6f6d969243cea7406b3fe0a095b92f
Plain Jane <jane99hsu@gmail.com>
2021-01-16 15:28:19 -0800

    added all required npm packages

--- server/server.js
+++ server/server.js
@@ -3,8 +3,27 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+
+// import typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+
+// import authentication middleware
+const { authMiddleware } = require('./utils/auth');
+
+// create new Apollo server and pass in schema data and authentication
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// integrate Apollo server with the Express application 
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,6 +35,13 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
+// serve up the React front-end code in production if we make a GET request to any location on the server that doesn't have an explicit route defined
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  // log where we can go to test our GQL API
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
